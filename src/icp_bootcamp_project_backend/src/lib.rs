7238b19e244a79632da2014c4870d99a36377113
xubuntu <kamil.borkow@wp.pl>
2024-07-13 16:54:30 +0200

    started backend

--- src/icp_bootcamp_project_backend/src/lib.rs
+++ src/icp_bootcamp_project_backend/src/lib.rs
@@ -1,4 +1,19 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static LEADERBOARD: RefCell<Vec<(String, u64)>> = RefCell::default();
+}
+
+#[ic_cdk::update]
+fn add_record(name: String, score: u64){
+    LEADERBOARD.with(|records|{
+        records.borrow_mut().push((name, score));
+    });
+}
+
 #[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+fn read_leaderboard() -> Vec<(String, u64)> {
+    LEADERBOARD.with(|records|{
+        records.borrow().clone()
+    })
 }
